4b9af2e44c47a60501f0c6535ddb892dacb1b113
samim1920 <sksamimali2021@gmail.com>
2026-04-29 20:32:41 +0530

    add

--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -7,7 +7,7 @@ function Course() {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("http://localhost:4001/book");
+        const res = await axios.get("https://bookstoreapp56.onrender.com/book");
         console.log(res.data);
         setBook(res.data);
       } catch (error) {
@@ -21,7 +21,7 @@ function Course() {
       <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
         <div className="mt-28 items-center justify-center text-center">
           <h1 className="text-2xl  md:text-4xl">
-            We're delighted to have you{" "}
+            We are delighted to have you{" "}
             <span className="text-pink-500"> Here! :)</span>
           </h1>
           <p className="mt-12">
